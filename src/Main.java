4baecefcb5ae77e248809270078a168edd598c3e
613668 <beres.613668@swl.k12.oh.us>
2023-09-28 09:20:35 -0400

    gdg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //variables
+        int age;
+
+        System.out.println("Enter your age: ");
+        if (scan.hasNextInt()) {
+            age = scan.nextInt();
+            if (age >= 21) {
+                System.out.println("You get a wristband!!");
+            } else {
+                System.out.println("GO AWAY YOU CANNOT HAVE A WRIST BAND!!!!!");
+            }
+        } else {
+            System.out.println("THAT'S NOT EVEN A NUMBER!!!!!!!!");
+        }
     }
 }
